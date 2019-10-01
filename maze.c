b455480cdeb1d2b3d2c0868df48d381463629cbe
bgb10 <bgb10@naver.com>
2019-10-01 10:35:14 +0900

    Using a 1-dimension array with binary shifting and unsigned variable(array memory 34byte)

--- maze.c
+++ maze.c
@@ -1,19 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void print_maze(char hor_l[], char ver_l[]);
+void print_maze(unsigned short hor[], unsigned char ver[]);
 
 /*
-binary는 0b로 쓴다. shifting 연산 사용?? 
+binary는 0b로 쓴다. shifting 연산 사용??
 매 줄마다 그냥 0b형태로 데이터 저장한 후에 2진수 단위마다 수들을 가져와서 1이면 출력하면 된다.
 데이터 범위는 0 ~ 등비급수 이용해서 나온 값
 하지만 쪼개다보면 뭐 바이트 수를 더 줄일 수 있지만, 효율적이지는 않다.
 1. shift연산을 사용해서 자릿수를 다 내려버리는 형태로?
-2. 나머지연산을 array에 받아서(그럼 반대모양의 array가 나오겠지) 출력 
+2. 나머지연산을 array에 받아서(그럼 반대모양의 array가 나오겠지) 출력
+
+//그래도 잘했다. 오류를 여기저기 왔다갔다 하면서 찾아보니까.
+//ver_l은 알고리즘 문제가 아니라 배열 문제였다. 
+//총 34byte
 */
 int main()
 {
-	unsigned short hor[11] = {0b011011000, 
+    unsigned char ver[12] =  {0b00010001,
+							  0b00110011,
+							  0b00101011,
+							  0b11111101,
+							  0b11010010,
+							  0b01100001,
+							  0b00110010,
+							  0b10011001,
+							  0b10001000,
+							  0b00101011,
+							  0b01111001,
+							  0b01010100};
+
+	unsigned short hor[11] = {0b011011000,
 							  0b110001000,
 							  0b010110100,
 							  0b001001010,
@@ -24,24 +41,13 @@ int main()
 							  0b011011010,
 							  0b010000100,
 							  0b100000110};
-	
-	char ver[12] = 			 {0b00010001, 
-							  0b00110011,
-							  0b00101011,
-							  0b11111101,
-							  0b11010010,
-							  0b01100001,
-							  0b00110010,
-							  0b10011001,
-							  0b00101001,
-							  0b01110001,
-							  0b01010100};
 
+    //printf("%d %d", hor[0], ver[0]);
 	print_maze(hor, ver);
 	return 0;
 }
 
-void print_maze(char hor, char ver)
+void print_maze(unsigned short hor[11], unsigned char ver[12])
 {
 	int x = 0, y = 0;
 	int digit_ver, digit_hor;
@@ -57,19 +63,18 @@ void print_maze(char hor, char ver)
 
 		for (int  x = 0; x < 8; x++)
 		{
-			for(int shift = 8; shift > 0; shift--)
-			{
-				digit_ver /= 2;
-			}
+			digit_ver >>= (7-x); //shift연산도 =을 붙여야 함.
 			digit_ver %= 2;
+			//printf("%d\n", digit_ver); //ver만 문제가 있다. ver 입력은 잘 됨.
 			if (digit_ver == 1) //digit이 1이면
 				printf("   |");
 			else //digit이 0이면
 				printf("    ");
 
+            digit_ver = ver[y];
 		}
 		printf("   |\n");
-		
+
 		//hor자리
 
 		printf("+");
@@ -78,39 +83,36 @@ void print_maze(char hor, char ver)
 
 		for (int x = 0; x < 9; x++)
 		{
-			for(int shift = 9; shift > 0; shift--)
-			{
-				digit_hor /= 2;
-			}
+			digit_hor >>= (8-x); //shift연산도 =을 붙여야 함.
 			digit_hor %= 2;
-
 			if (digit_hor == 1)
 				printf("---+");
 			else
 				printf("   +");
 
+            digit_hor = hor[y];
 		}
 		printf("\n");
 	}
-	
+
 	//ver11줄
+
 	printf("|");
 
 	digit_ver = ver[11];
 
 	for (int x = 0; x < 8; x++)
 	{
-		for(int shift = 8; shift > 0; shift--)
-			{
-				digit_ver /= 2;
-			}
+			digit_ver >>= (7-x); //shift연산도 =을 붙여야 함.
 			digit_ver %= 2;
 			if (digit_ver == 1) //digit이 1이면
 				printf("   |");
 			else //digit이 0이면
 				printf("    ");
+
+            digit_ver = ver[11];
 	}
-	
+
 	printf("   |\n");
 	printf("+---+---+---+---+---+---+---+---+---+");
 }
